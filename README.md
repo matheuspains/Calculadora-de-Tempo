81df2778fd379f8372679528f163ce6222230ae3
matheuspains <pains@discente.ufg.br>
2023-12-14 08:10:08 -0300

    datas fixas e botão de remover

---
+++ README.md
@@ -0,0 +1,32 @@
+# Calculadora de Tempo Restante
+
+Este é um projeto simples de uma calculadora de tempo restante que permite aos usuários escolher uma data futura e calcular o tempo restante até essa data. O projeto inclui uma página HTML com um campo de entrada de data, um botão de cálculo e uma área para exibir o resultado. A estilização é feita usando CSS e a lógica de cálculo é implementada em JavaScript.
+
+## Funcionalidades
+
+- Escolha uma data futura no campo de entrada de data.
+- Clique no botão "Calcular" para calcular o tempo restante até a data escolhida.
+- O resultado será exibido abaixo do botão de cálculo.
+
+## Instruções de Uso
+
+1. Escolha uma data futura no campo de entrada de data.
+2. Clique no botão "Calcular" para ver o tempo restante até a data escolhida.
+
+## Como Contribuir
+
+Sinta-se à vontade para contribuir, reportar problemas ou fazer melhorias neste projeto. Basta seguir estas etapas:
+
+1. Faça um fork do projeto.
+2. Crie uma branch para suas alterações: `git checkout -b nome-da-sua-branch`
+3. Faça commit das suas alterações: `git commit -m 'Descrição das alterações'`
+4. Faça push para a branch: `git push origin nome-da-sua-branch`
+5. Abra um pull request.
+
+## Licença
+
+Este projeto é distribuído sob a licença [MIT](LICENSE). Para saber mais sobre o que isso significa, leia [a explicação sobre a Licença MIT](https://opensource.org/licenses/MIT).
+
+---
+
+**Observação:** Certifique-se de substituir "nome-da-sua-branch" pelos detalhes relevantes quando estiver contribuindo.
--- script.js
+++ script.js
@@ -1,19 +1,126 @@
-function calcularTempoRestante() {
-    var dataAtual = new Date();
-    var dataInputUsuario = document.getElementById("dataInput").value;
-    var dataInput = new Date(dataInputUsuario + "T00:00:00");
+// Array para armazenar as datas inseridas
+var datasInseridas = [];
 
-    if (isNaN(dataInput.getTime())) {
-        alert("Formato de data inválido. Selecione uma data no calendário.");
+function adicionarFeriados() {
+    // Lista de feriados em 2024
+    var feriados2024 = [
+        { nome: 'Ano-Novo', data: new Date('2024-01-01') },
+        { nome: 'Carnaval', data: new Date('2024-02-12') },
+        { nome: 'Quarta-feira de Cinzas', data: new Date('2024-02-14') },
+        { nome: 'Sexta-feira Santa', data: new Date('2024-03-29') },
+        { nome: 'Páscoa', data: new Date('2024-03-31') },
+        { nome: 'Dia do Trabalho', data: new Date('2024-05-01') },
+        { nome: 'Corpus Christi', data: new Date('2024-06-20') },
+        { nome: 'Independência do Brasil', data: new Date('2024-09-07') },
+        { nome: 'Nossa Senhora Aparecida', data: new Date('2024-10-12') },
+        { nome: 'Finados', data: new Date('2024-11-02') },
+        { nome: 'Proclamação da República', data: new Date('2024-11-15') },
+        { nome: 'Natal', data: new Date('2024-12-25') }
+    ];
+
+    // Obter a lista de feriados
+    var feriadosList = document.getElementById('feriados');
+
+    // Limpar a lista de feriados
+    feriadosList.innerHTML = '';
+
+    // Adicionar cada feriado à lista
+    feriados2024.forEach(function (feriado) {
+        // Adiciona um dia à data para evitar a subtração que estava acontecendo
+        var dataFormatada = new Date(feriado.data.getTime() + (24 * 60 * 60 * 1000));
+        
+        var item = document.createElement('li');
+        item.innerHTML = `${feriado.nome}: ${dataFormatada.toDateString()}`;
+        feriadosList.appendChild(item);
+    });
+}
+
+// Função para adicionar data
+function adicionarData() {
+    var nomeInput = document.getElementById('nomeInput');
+    var dataInput = document.getElementById('dataInput');
+
+    // Obter os valores dos campos
+    var nome = nomeInput.value;
+    var dataString = dataInput.value;
+
+    // Validar se ambos os campos estão preenchidos
+    if (nome.trim() === '' || dataString.trim() === '') {
+        alert('Por favor, preencha ambos os campos.');
         return;
     }
 
-    var diff = Math.abs(dataInput - dataAtual);
-    var diasRestantes = Math.floor(diff / (1000 * 60 * 60 * 24));
-    var horasRestantes = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutosRestantes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-    var segundosRestantes = Math.floor((diff % (1000 * 60)) / 1000);
+    // Converter a string da data para um objeto Date
+    var dataEscolhida = new Date(dataString);
 
-    var resultado = diasRestantes + " dias, " + horasRestantes + " horas, " + minutosRestantes + " minutos e " + segundosRestantes + " segundos restantes.";
-    document.getElementById("resultado").innerHTML = "Tempo restante até " + dataInputUsuario + ": " + resultado;
+    // Verificar se a data inserida é válida
+    if (isNaN(dataEscolhida.getTime())) {
+        alert('Por favor, insira uma data válida.');
+        return;
+    }
+
+    // Adicionar a data ao array com um ID único
+    var dataInserida = {
+        id: Date.now(), // Usando timestamp como ID unico
+        nome: nome,
+        data: dataEscolhida
+    };
+    datasInseridas.push(dataInserida);
+
+    // Limpar os campos de entrada
+    nomeInput.value = '';
+    dataInput.value = '';
+
+    // Atualizar a exibição das datas inseridas
+    exibirDatasInseridas();
 }
+
+// Função para exibir datas inseridas
+function exibirDatasInseridas() {
+    var resultado = document.getElementById('resultado');
+    resultado.innerHTML = '';
+
+    // Iterar sobre as datas inseridas e exibi-las
+    datasInseridas.forEach(function (item) {
+        // Calcular a diferença de tempo para cada data inserida
+        var diferencaTempo = item.data.getTime() - new Date().getTime();
+        var dias = Math.floor(diferencaTempo / (1000 * 60 * 60 * 24));
+        var horas = Math.floor((diferencaTempo % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutos = Math.floor((diferencaTempo % (1000 * 60 * 60)) / (1000 * 60));
+        var segundos = Math.floor((diferencaTempo % (1000 * 60)) / 1000);
+
+        var resultadoItem = document.createElement('div');
+        resultadoItem.className = 'resultado-item';
+
+        // Exibir o resultado e adicionar o botão de remover
+        resultadoItem.innerHTML = `Faltam ${dias} dias, ${horas} horas, ${minutos} minutos e ${segundos} segundos para ${item.nome}.`;
+        var botaoRemover = document.createElement('button');
+        botaoRemover.textContent = 'Remover';
+        botaoRemover.onclick = function () {
+            removerData(item.id);
+        };
+        resultadoItem.appendChild(botaoRemover);
+
+        // Appendar o resultado
+        resultado.appendChild(resultadoItem);
+    });
+}
+
+// Função para remover data
+function removerData(id) {
+    // Encontrar o índice da data pelo id
+    var index = datasInseridas.findIndex(function (item) {
+        return item.id === id;
+    });
+
+    // Verificar se o id foi encontrado
+    if (index !== -1) {
+        // Remover a data do array pelo índice
+        datasInseridas.splice(index, 1);
+        // Atualizar a exibição das datas inseridas
+        exibirDatasInseridas();
+    }
+}
+
+// Chamar a função para adicionar feriados
+adicionarFeriados();
